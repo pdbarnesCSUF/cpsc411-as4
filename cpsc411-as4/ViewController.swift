14f391418645956ee4ca50ff24b2b5acabde4f41
CampusUser <campususer@csufmac-009.local>
2017-04-10 19:38:15 -0700

    Initial Commit

---
+++ cpsc411-as4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cpsc411-as4
+//
+//  Created by CampusUser on 4/10/17.
+//  Copyright © 2017 pdbarnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
