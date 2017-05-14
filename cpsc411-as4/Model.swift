d8ddaa1c2e145239d4a00fbfb5ca0c795ba9b159
CampusUser <campususer@csufmac-009.barnes>
2017-05-13 19:56:13 -0700

    added model added more buttons and labels added view controller for fav

---
+++ cpsc411-as4/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  cpsc411-as4
+//
+//  Created by CampusUser on 5/13/17.
+//  Copyright © 2017 pdbarnes. All rights reserved.
+//
+
+import Foundation
+
+class CalcData
+{
+    public static var shared:CalcData = CalcData()
+    
+    init()
+    {
+        currencyArr = ["USD","EUR","GBP","INR","AUD","CAD","SGD","JPY","TWD","CNY"]
+        booleanArr = [true,true,true,true,true,true,true,true,true,true]
+    }
+    //favs
+    public var currencyArr: [String]
+    public var booleanArr: [Bool]
+    
+}
