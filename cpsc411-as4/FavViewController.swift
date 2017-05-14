abc121f3cbdb3608d43d8df6b6288652497adc21
CampusUser <campususer@csufmac-009.barnes>
2017-05-13 23:30:12 -0700

    buttons get current status

--- cpsc411-as4/FavViewController.swift
+++ cpsc411-as4/FavViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class FavViewController: UIViewController {
 
+    @IBOutlet weak var usdSwitch: UISwitch!
+    @IBOutlet weak var eurSwitch: UISwitch!
+    @IBOutlet weak var gbpSwitch: UISwitch!
+    @IBOutlet weak var inrSwitch: UISwitch!
+    @IBOutlet weak var audSwitch: UISwitch!
+    @IBOutlet weak var cadSwitch: UISwitch!
+    @IBOutlet weak var sgdSwitch: UISwitch!
+    @IBOutlet weak var jpySwitch: UISwitch!
+    @IBOutlet weak var twdSwitch: UISwitch!
+    @IBOutlet weak var cnySwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        usdSwitch.isOn = CalcData.shared.booleanArr[0]
+        eurSwitch.isOn = CalcData.shared.booleanArr[1]
+        gbpSwitch.isOn = CalcData.shared.booleanArr[2]
+        inrSwitch.isOn = CalcData.shared.booleanArr[3]
+        audSwitch.isOn = CalcData.shared.booleanArr[4]
+        cadSwitch.isOn = CalcData.shared.booleanArr[5]
+        sgdSwitch.isOn = CalcData.shared.booleanArr[6]
+        jpySwitch.isOn = CalcData.shared.booleanArr[7]
+        twdSwitch.isOn = CalcData.shared.booleanArr[8]
+        cnySwitch.isOn = CalcData.shared.booleanArr[9]
         // Do any additional setup after loading the view.
     }
 
@@ -20,8 +39,10 @@ class FavViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
+    private func updateCalc()
+    {
+        
+    }
     /*
     // MARK: - Navigation
 
